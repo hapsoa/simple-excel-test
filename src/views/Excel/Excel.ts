0478bf4520afa81be74631088c7e737b1ee7358f
Jaejong Heo <hapsoa@gmail.com>
2019-11-02 23:47:25 +0900

    update excel test

--- src/views/Excel/Excel.ts
+++ src/views/Excel/Excel.ts
@@ -1,10 +1,71 @@
-// file created at 2019-11-2
-// Auto-generated files Excel.ts
-
 import { Vue, Component } from 'vue-property-decorator';
 
 @Component({})
 export default class Excel extends Vue {
+  public $refs!: {
+    imageInput: any;
+  };
+
+  private name: string = '';
+  private testJsonObject = {
+    a: 'test',
+    b: 'good'
+  };
+
+  private showTestJsonObject() {
+    console.log('testJsonObject', this.testJsonObject);
+  }
+
+  private pickFile() {
+    this.$refs.imageInput.click();
+  }
+
+  private onFilePicked(e: any) {
+    console.log('onFilePicked');
+    const files = e.target.files;
+
+    if (files[0] !== undefined) {
+      // files[0].name;
+      const fileReader = new FileReader();
+      fileReader.readAsText(files[0]);
+      fileReader.addEventListener('load', () => {
+        // csv형식을 string text로 만든다.
+        const csvText: string = fileReader.result as string;
+        // console.log('csvText', csvText);
+
+        // csv string을 json으로 바꾼다.
+        const jsonResult = this.csvJSON(csvText);
+        console.log('jsonResult', jsonResult);
+      });
+    }
+  }
+
+  /**
+   * csv(string)형식의 text를 json으로 변환해주는 함수이다.
+   * @param csv the CSV file with headers
+   */
+  private csvJSON(csv: string) {
+    const lines = csv.split('\n');
+
+    const result: any[] = [];
+
+    const headers = lines[0].split(',');
+
+    for (let i = 1; i < lines.length; i++) {
+      const obj = {};
+      const currentline = lines[i].split(',');
+
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+    }
+
+    return result; // JavaScript object
+    // return JSON.stringify(result); // JSON
+  }
+
   private mounted() {
     //
   }
--- src/views/Excel/Excel.vue
+++ src/views/Excel/Excel.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1>Excel</h1>
+  <v-container pa-0 ma-0>
+    <v-row>
+      <v-col>
+        <!-- <v-text-field label="Select Image" @click="pickFile" v-model="name" required readonly></v-text-field> -->
+        <v-btn @click="pickFile">upload csv</v-btn>
+        <input
+          type="file"
+          style="display: none"
+          ref="imageInput"
+          @change="onFilePicked"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <table>
+          <tr>
+            <input type="text" v-model="testJsonObject.a" />
+            <td>오</td>
+          </tr>
+        </table>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-btn @click="showTestJsonObject">show TestJsonObject</v-btn>
+    </v-row>
+  </v-container>
 </template>
 <script src="./Excel.ts"></script>
 <style lang="scss" scoped>
